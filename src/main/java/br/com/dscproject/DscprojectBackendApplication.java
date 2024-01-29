a18a04d1d7d439ebf1db181a92ed2b080201b43d
Diego Cordeiro <sgt.chacal.d@gmail.com>
2024-01-28 23:19:38 -0300

    Adicionando Springboot 3.2.2 com JAVA 21

---
+++ src/main/java/br/com/dscproject/DscprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.dscproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscprojectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dscproject/DscprojectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dscproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscprojectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
